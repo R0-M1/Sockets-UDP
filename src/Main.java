bd507f4e20b222e2ed65a3ac696da54b617275f8
Romain <romain.aillaud@free.fr>
2025-02-26 17:12:36 +0100

    scanner de port UDP sur localhost

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int startPort = 100;
+        int endPort = 1000;
+        scanPorts(startPort, endPort);
+    }
+
+    public static void scanPorts(int startPort, int endPort) {
+        HashMap<Integer, String> etatPorts = new HashMap<>();
+        for (int port = startPort; port <= endPort; port++) {
+            if (isPortOpen(port)) {
+                etatPorts.put(port, "ouvert");
+            } else {
+                etatPorts.put(port, "fermé");
+            }
+        }
+        System.out.println(etatPorts);
+    }
+
+    private static boolean isPortOpen(int port) {
+        try (DatagramSocket socket = new DatagramSocket(port)) {
+            return false;
+        } catch (SocketException e) {
+            return true;
+        }
+    }
+}
